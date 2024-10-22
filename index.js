9886b937c2a18d16f398d473ad451101e807eb62
soodala2003 <soodala78@gmail.com>
2024-10-22 13:54:49 -0400

    updated the index.js file

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ import axios from "axios";
 const API_KEY =
   "live_2L5qpy6HjWEc4qxT1JVDCifdbhbUKnvSXv3S5Awwj7ygiHvXZgwvqCPjpaBr0tvS";
 
-const url = "https://api.thecatapi.com/v1/breeds";
+const URL = "https://api.thecatapi.com/v1/breeds";
 let storedBreeds = [];
 
 async function initialLoad() {
     console.log("Request begins: ");
-    const response = await axios.get(url, {
+    const response = await axios.get(URL, {
       headers: {
         "x-api-key": API_KEY,
       },
     });
-  
-    storedBreeds = response.data;
+    
+    //storedBreeds = response.data;
+    //console.log(storedBreeds[0]);
+    for (let i = 0; i < response.data.length; i++) {
+      storedBreeds.push(response.data[i]); 
+    }
+    console.log(storedBreeds.length);
+    console.log(storedBreeds[0]);
+    return storedBreeds;
 }
+
 initialLoad();
-console.log(storedBreeds[0]);
+console.log(storedBreeds.length);
