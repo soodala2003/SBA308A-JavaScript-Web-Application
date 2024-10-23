581fa1ff93d538c35fe09622626026eef49aaba5
soodala2003 <soodala78@gmail.com>
2024-10-23 14:53:45 -0400

    updated main.js file

--- main.js
+++ main.js
@@ -4,7 +4,22 @@ const breedSelect = document.getElementById("breedSelect");
 const infoDump = document.getElementById("infoDump");
 const getFavouritesBtn = document.getElementById("getFavouritesBtn");
 const carousel = document.getElementById("carouselInner");
-const h4 = document.querySelector("h4");
+const parentEl = document.getElementById("carouselInner");
+const h6 = document.querySelector("h6");
+
+const ul = document.createElement("ul");
+const li1 = document.createElement("li");
+const li2 = document.createElement("li");
+const li3 = document.createElement("li");
+const li4 = document.createElement("li");
+const li5 = document.createElement("li");
+const wikiLink = document.createElement("a");
+
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
+ul.appendChild(li4);
+ul.appendChild(li5);
 
 const API_KEY =
     "live_2L5qpy6HjWEc4qxT1JVDCifdbhbUKnvSXv3S5Awwj7ygiHvXZgwvqCPjpaBr0tvS";
@@ -13,15 +28,13 @@ let storedBreeds = [];
 
 async function initialLoad() {
     try {
-        //console.log("Request begins: ");
-        const response = await axios.get(URL, {
-            headers: {
-              "x-api-key": API_KEY,
-            },
-        });
-    
-        storedBreeds = response.data;
-        //console.log(storedBreeds[0]);
+        const response = await fetch(URL, {headers: {
+            "x-api-key": API_KEY
+        }});
+        const jsonData = await response.json();
+        storedBreeds = jsonData;
+        
+        console.log(storedBreeds[0]);
         /*
         console.log(storedBreeds.length);
         console.log(storedBreeds[0]);
@@ -43,7 +56,7 @@ async function initialLoad() {
 }
 
 initialLoad();
-console.log(storedBreeds.length);
+//console.log(storedBreeds.length);
 
 // Create the initial carousel.
 function createCarousel() {
@@ -61,8 +74,8 @@ function createCarousel() {
 getFavouritesBtn.addEventListener("click", function() {
     const selectedBreedId = breedSelect.value;
     const selectedBreedIndex = breedSelect.selectedIndex;
-    console.log(selectedBreedId);
-    console.log(selectedBreedIndex);
+    //console.log(selectedBreedId);
+    //console.log(selectedBreedIndex);
 
     let selectedBreed = storedBreeds[selectedBreedIndex]; 
     console.log(selectedBreed.name);
@@ -70,11 +83,24 @@ getFavouritesBtn.addEventListener("click", function() {
 
     carouselElement.setAttribute("class", "carousel-item active");
     Carousel.appendCarousel(carouselElement);
-    h4.innerHTML = selectedBreed.name;
 
-    let infoLists = document.createElement("div");
+    h6.innerHTML = selectedBreed.name;
+    h6.appendChild(ul);
+    li1.innerHTML = `<p>Description: ${selectedBreed.description}</p>`;
+    li2.innerHTML = `<p>Temperament: ${selectedBreed.temperament}</p>`;
+    li3.innerHTML = `<p>Origin: ${selectedBreed.origin}</p>`;
+    li4.innerHTML = `<p>Life Span: ${selectedBreed.life_span}</p>`;
+    li5.innerHTML = `<p>More Information: </p>`;//${selectedBreed.wikipedia_url}</p>`;
+    wikiLink.setAttribute("href", `${selectedBreed.wikipedia_url}`);
+    wikiLink.textContent = `${selectedBreed.wikipedia_url}`;
+    li5.appendChild(wikiLink);
+
+    // Reset the select element
+    breedSelect.selectedIndex = -1;
+
+    /* let infoLists = document.createElement("div");
     infoLists.innerHTML = `<p>Description: ${selectedBreed.description}</p>`;
-    h4.appendChild(infoLists);
+    h6.appendChild(infoLists); */
 
 });
 
