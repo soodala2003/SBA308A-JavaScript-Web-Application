14031b6646d0eb30950314477c2eaf32fc5949f6
soodala2003 <soodala78@gmail.com>
2024-10-26 02:09:23 -0400

    added 'resultsBtn.addEventListener' in Votes.js file

--- Votes.js
+++ Votes.js
@@ -1,8 +1,9 @@
-import { voteUp, voteDown } from "./main.js";
+import { voteUp, voteDown, getVotesByUserId } from "./main.js";
 
 export async function voteImage(imgId, subId) {
     const upBtn = document.getElementById("voteUpBtn");
     const downBtn = document.getElementById("voteDownBtn"); 
+    const resultsBtn= document.getElementById("voteResultsBtn");
     
     upBtn.addEventListener("click", () => {
         voteUp(imgId, subId);
@@ -10,6 +11,32 @@ export async function voteImage(imgId, subId) {
 
     downBtn.addEventListener("click", () => {
         voteDown(imgId, subId);
+    });
+
+    resultsBtn.addEventListener("click", () => {
+        document.querySelector("d-flex flex-row").style.display = "none";
+        document.getElementById("vote-options").style.display = "block";
+        //let results = 
+        getVotesByUserId(subId).then((data) => {
+            data.map(function(voteData) {
+                const imageData = voteData.image;
+                let image = document.createElement("img");
+                image.src = imageData.url;
+
+                let gridCell = document.createElement("div");
+                if (voteData.value < 0) {
+                    gridCell.classList.add("red");
+                } else {
+                    gridCell.classList.add("green");
+                }
+
+                gridCell.classList.add("col-lg");
+                gridCell.appendChild(image);
+                document.getElementById("grid").appendChild(gridCell);
+            });
+        }).catch(error => {
+            console.log(error);
+        });
     });
 
     //return vote;
